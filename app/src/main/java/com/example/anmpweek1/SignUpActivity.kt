74fe4a1ee7fbe9dab24d084b06e60f355714ad8d
Anri20 <88647934+Anri20@users.noreply.github.com>
2024-02-22 18:42:55 +0700

    SignUpActivity Comment

--- app/src/main/java/com/example/anmpweek1/SignUpActivity.kt
+++ app/src/main/java/com/example/anmpweek1/SignUpActivity.kt
@@ -7,5 +7,10 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+//        SignUp Comment
+//        1.
+//        2.
+//        3.
     }
 }
